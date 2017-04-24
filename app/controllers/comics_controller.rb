ad3ff9297e4dc66c740a5ed89f726dd4ccaf7fae
Surf Thomas <sthomas@jackthreads.com>
2017-04-24 13:27:58 -0400

    view and controller logic

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,3 +1,4 @@
+require 'mechanize'
 class ComicsController < ApplicationController
   def index
     @comics = Comic.all
@@ -5,6 +6,17 @@ class ComicsController < ApplicationController
       x.json {render json: @comics}
       x.html
     end
+  end
+  def search
+  	agent = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
+  	elastic_map = ENV['ELASTIC_MAP']
+	elastic_search_url = ENV['ELASTIC_SEARCH']
+  	value = agent.post("#{elastic_search_url}#{elastic_map}_search", {"query"=>{"match"=>{"series"=>{"query"=>"#{search_params["query"]}","fuzziness"=>"AUTO"}}}}.to_json, {'Content-Type' => 'application/json'})
+  	render json: value.to_json
+  end
 
+  private
+  def search_params
+  	params.permit(:query)
   end
 end
